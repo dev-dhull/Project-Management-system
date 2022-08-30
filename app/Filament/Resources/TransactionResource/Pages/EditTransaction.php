890e44eecfd1c031cd49ce779fd8aea7ddb73b9a
David <103427573+dev-dhull@users.noreply.github.com>
2022-08-30 10:04:14 +0530

    changes

--- app/Filament/Resources/TransactionResource/Pages/EditTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/EditTransaction.php
@@ -10,6 +10,11 @@ class EditTransaction extends EditRecord
 {
     protected static string $resource = TransactionResource::class;
 
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
+
     protected function getActions(): array
     {
         return [
