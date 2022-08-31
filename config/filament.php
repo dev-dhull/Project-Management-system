39181a87906a4bbf07e70191f85e09031b4da52a
David <103427573+dev-dhull@users.noreply.github.com>
2022-08-31 19:04:54 +0530

    amt

--- config/filament.php
+++ config/filament.php
@@ -61,7 +61,7 @@ return [
     |
     */
 
-    'home_url' => '/',
+    'home_url' => '/admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -206,7 +206,7 @@ return [
             'alignment' => 'right',
         ],
         'sidebar' => [
-            'is_collapsible_on_desktop' => false,
+            'is_collapsible_on_desktop' => true,
             'groups' => [
                 'are_collapsible' => true,
             ],
